19737d26b0b4e3658bd26b1d31476ea03309e4d4
xxgingersnapsxx <zxgingersnapsxz@gmail.com>
2021-07-16 16:17:05 +0900

    JS - 수정된 테이블 VO 추가

--- src/damanna/vo/MemberVO.java
+++ src/damanna/vo/MemberVO.java
@@ -1,10 +1,8 @@
 package damanna.vo;
 
 public class MemberVO {
-	private String memJoin;
-	private String memRej;
-	private String memberId;
 	private int cityCode;
+	private String memberId;
 	private String memberPw;
 	private String memberNm;
 	private String memberBir;
@@ -13,25 +11,23 @@ public class MemberVO {
 	private String memberGen;
 	private String memberImg1;
 	private String memberImg2;
+	private String matchType;
 	private String memberHobby;
 	private String profileIntro;
 	private String memberInsta;
 	private String profileMusic;
-	private String matchType;
+	private String memJoin;
 	private String approveNy;
-	private String memberDrop;
+	private String memRej;
 	private String membershipYn;
 
 	// 회원 가입을 위한 생성자, DEFAULT 값이 있는 경우 자동입력되게 함
-	public MemberVO(String memJoin, String memRej, String memberId, int cityCode, String memberPw, String memberNm,
-			String memberBir, String membmerMail, String memberHp, String memberGen, String memberImg1,
-			String memberImg2, String memberHobby, String profileIntro, String memberInsta, String profileMusic,
-			String matchType) {
+	public MemberVO(int cityCode, String memberId, String memberPw, String memberNm, String memberBir,
+			String membmerMail, String memberHp, String memberGen, String memberImg1, String memberImg2,
+			String matchType, String memberHobby, String profileIntro, String memberInsta, String profileMusic) {
 		super();
-		this.memJoin = memJoin;
-		this.memRej = memRej;
-		this.memberId = memberId;
 		this.cityCode = cityCode;
+		this.memberId = memberId;
 		this.memberPw = memberPw;
 		this.memberNm = memberNm;
 		this.memberBir = memberBir;
@@ -40,22 +36,20 @@ public class MemberVO {
 		this.memberGen = memberGen;
 		this.memberImg1 = memberImg1;
 		this.memberImg2 = memberImg2;
+		this.matchType = matchType;
 		this.memberHobby = memberHobby;
 		this.profileIntro = profileIntro;
 		this.memberInsta = memberInsta;
 		this.profileMusic = profileMusic;
-		this.matchType = matchType;
 	}
-
-	public MemberVO(String memJoin, String memRej, String memberId, int cityCode, String memberPw, String memberNm,
-			String memberBir, String membmerMail, String memberHp, String memberGen, String memberImg1,
-			String memberImg2, String memberHobby, String profileIntro, String memberInsta, String profileMusic,
-			String matchType, String approveNy, String memberDrop, String membershipYn) {
+	
+	public MemberVO(int cityCode, String memberId, String memberPw, String memberNm, String memberBir,
+			String membmerMail, String memberHp, String memberGen, String memberImg1, String memberImg2,
+			String matchType, String memberHobby, String profileIntro, String memberInsta, String profileMusic,
+			String memJoin, String approveNy, String memRej, String membershipYn) {
 		super();
-		this.memJoin = memJoin;
-		this.memRej = memRej;
-		this.memberId = memberId;
 		this.cityCode = cityCode;
+		this.memberId = memberId;
 		this.memberPw = memberPw;
 		this.memberNm = memberNm;
 		this.memberBir = memberBir;
@@ -64,38 +58,15 @@ public class MemberVO {
 		this.memberGen = memberGen;
 		this.memberImg1 = memberImg1;
 		this.memberImg2 = memberImg2;
+		this.matchType = matchType;
 		this.memberHobby = memberHobby;
 		this.profileIntro = profileIntro;
 		this.memberInsta = memberInsta;
 		this.profileMusic = profileMusic;
-		this.matchType = matchType;
+		this.memJoin = memJoin;
 		this.approveNy = approveNy;
-		this.memberDrop = memberDrop;
-		this.membershipYn = membershipYn;
-	}
-
-	public String getMemJoin() {
-		return memJoin;
-	}
-
-	public void setMemJoin(String memJoin) {
-		this.memJoin = memJoin;
-	}
-
-	public String getMemRej() {
-		return memRej;
-	}
-
-	public void setMemRej(String memRej) {
 		this.memRej = memRej;
-	}
-
-	public String getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
+		this.membershipYn = membershipYn;
 	}
 
 	public int getCityCode() {
@@ -104,6 +75,14 @@ public class MemberVO {
 
 	public void setCityCode(int cityCode) {
 		this.cityCode = cityCode;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
 	}
 
 	public String getMemberPw() {
@@ -170,6 +149,14 @@ public class MemberVO {
 		this.memberImg2 = memberImg2;
 	}
 
+	public String getMatchType() {
+		return matchType;
+	}
+
+	public void setMatchType(String matchType) {
+		this.matchType = matchType;
+	}
+
 	public String getMemberHobby() {
 		return memberHobby;
 	}
@@ -202,12 +189,12 @@ public class MemberVO {
 		this.profileMusic = profileMusic;
 	}
 
-	public String getMatchType() {
-		return matchType;
+	public String getMemJoin() {
+		return memJoin;
 	}
 
-	public void setMatchType(String matchType) {
-		this.matchType = matchType;
+	public void setMemJoin(String memJoin) {
+		this.memJoin = memJoin;
 	}
 
 	public String getApproveNy() {
@@ -218,12 +205,12 @@ public class MemberVO {
 		this.approveNy = approveNy;
 	}
 
-	public String getMemberDrop() {
-		return memberDrop;
+	public String getMemRej() {
+		return memRej;
 	}
 
-	public void setMemberDrop(String memberDrop) {
-		this.memberDrop = memberDrop;
+	public void setMemRej(String memRej) {
+		this.memRej = memRej;
 	}
 
 	public String getMembershipYn() {
---
+++ src/damanna/vo/ProfileReportVO.java
@@ -0,0 +1,60 @@
+package damanna.vo;
+
+public class ProfileReportVO {
+	private String reportId;
+	private String memberReported;
+	private String memberId;
+	private String blackReason;
+	private String blackDate;
+
+	public ProfileReportVO(String reportId, String memberReported, String memberId, String blackReason,
+			String blackDate) {
+		super();
+		this.reportId = reportId;
+		this.memberReported = memberReported;
+		this.memberId = memberId;
+		this.blackReason = blackReason;
+		this.blackDate = blackDate;
+	}
+
+	public String getReportId() {
+		return reportId;
+	}
+
+	public void setReportId(String reportId) {
+		this.reportId = reportId;
+	}
+
+	public String getMemberReported() {
+		return memberReported;
+	}
+
+	public void setMemberReported(String memberReported) {
+		this.memberReported = memberReported;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getBlackReason() {
+		return blackReason;
+	}
+
+	public void setBlackReason(String blackReason) {
+		this.blackReason = blackReason;
+	}
+
+	public String getBlackDate() {
+		return blackDate;
+	}
+
+	public void setBlackDate(String blackDate) {
+		this.blackDate = blackDate;
+	}
+
+}
